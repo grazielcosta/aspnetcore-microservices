dd969b60648ad906dded27b4659e52af1da0f6ed
grazielbarbosa <grazielco@gmail.com>
2022-03-08 00:06:02 -0300

    Basket.API consuming Discount.Grpc

---
+++ src/Services/Basket/Basket.API/GrpcServices/DiscountGrpcService.cs
@@ -0,0 +1,21 @@
+﻿using Discount.Grpc.Protos;
+using System;
+using System.Threading.Tasks;
+
+namespace Basket.API.GrpcServices
+{
+    public class DiscountGrpcService
+    {
+        private readonly DiscountProtoService.DiscountProtoServiceClient _discountProtoServiceClient;
+
+        public DiscountGrpcService(DiscountProtoService.DiscountProtoServiceClient discountProtoServiceClient)
+        {
+            _discountProtoServiceClient = discountProtoServiceClient ?? throw new ArgumentNullException(nameof(discountProtoServiceClient));
+        }
+        public async Task<CouponModel> GetDiscount(string productName)
+        {
+            var discounRequest = new GetDiscountRequest { ProductName = productName };
+            return await _discountProtoServiceClient.GetDiscountAsync(discounRequest);
+        }
+    }
+}
