eb2635af053649cbb7ace3550cf6566d4ddd2614
grazielbarbosa <grazielco@gmail.com>
2022-03-11 14:30:41 -0300

    MigrateDatabase in Ordering.API and Ordering.Infraestructure

---
+++ src/Services/Ordering/Ordering.Infrastructure/Repositories/OrderRepository.cs
@@ -0,0 +1,20 @@
+﻿using Ordering.Application.Contracts.Persistence;
+using Ordering.Domain.Entities;
+using Ordering.Infrastructure.Persistence;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace Ordering.Infrastructure.Repositories
+{
+    public class OrderRepository : RepositoryBase<Order>, IOrderRepository
+    {
+        public OrderRepository(OrderContext dbContext) : base(dbContext)
+        {
+        }
+
+        public async Task<IEnumerable<Order>> GetOrdersByUserName(string userName)
+        {
+            return await base.GetAsync(o => o.UserName == userName);
+        }
+    }
+}
